2a390c53fc5a781fde6d60df9d616de9becd3e4f
Nathan Stark <git@starlabs.su>
2018-01-16 13:32:12 +0500

    Автозагрузчик классов согласно стандарту PSR-4

--- web/local/php_interface/init.php
+++ web/local/php_interface/init.php
@@ -1,5 +1,16 @@
 <?php
 
+// автозагрузчик для всех классов проекта с соблюдением стандарта PSR-4
+spl_autoload_register(function ($class) {
+    $class = str_replace('\\', '/', $class);
+
+    $path = $_SERVER["DOCUMENT_ROOT"] .'/local/php_interface/lib/'.$class.'.php';
+
+    if (is_readable($path)) {
+        require_once $path;
+    }
+});
+
 require_once __DIR__.'/include/define.php';     # Константы
 require_once __DIR__.'/include/class.php';      # Общие классы
 require_once __DIR__.'/include/functions.php';  # Общие функции
